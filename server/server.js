5acb6871b2e7685084e344fa0c9f6e078b4cd3b4
DPEDITS <debashishparida75@gmail.com>
2025-07-31 22:14:28 +0530

    Added MongoDB connection successfully

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
+import { connectDB } from "./lib/db.js";
 //Create Express App and HTTP Server
 const app=express();
 const server=http.createServer(app);
@@ -9,5 +10,8 @@ const server=http.createServer(app);
 app.use(express.json({limit: "4mb"}));
 app.use(cors());
 app.use("/api/status",(req,res)=>res.send("Server is running"));
+
+//Connect To MONGODB
+await connectDB();
 const PORT= process.env.PORT || 5000;
 server.listen(PORT,()=>console.log("Sever is running on PORT:"+PORT));
