362a2690e9ffb04e121abeb9d5b0b2d1fde87db1
DPEDITS <debashishparida75@gmail.com>
2025-08-01 09:19:33 +0530

    Added Routers for auth

--- server/server.js
+++ server/server.js
@@ -3,14 +3,16 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
 //Create Express App and HTTP Server
 const app=express();
 const server=http.createServer(app);
 //Middleware Setup
 app.use(express.json({limit: "4mb"}));
 app.use(cors());
+//Routes Setup
 app.use("/api/status",(req,res)=>res.send("Server is running"));
-
+app.use("/api/auth",userRouter);
 //Connect To MONGODB
 await connectDB();
 const PORT= process.env.PORT || 5000;
